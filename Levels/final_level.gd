bacc5d2d358818b6029c70f4ef5fb0a39a9f811a
Dcode202 <49726802+Dcode202@users.noreply.github.com>
2023-10-01 01:42:03 +0530

    update

--- Levels/final_level.gd
+++ Levels/final_level.gd
@@ -1,14 +1,15 @@
 extends Node
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	SignalBus.connect("boss_defeated", final_level)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+func final_level():
+	get_tree().change_scene_to_file("res://Levels/game_completed.tscn")
 
-func _on_door_body_entered(body):
-	if body.name == "Player":
-		get_tree().change_scene_to_file("res://Levels/game_completed.tscn")
+
